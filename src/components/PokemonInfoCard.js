db6e0d3ad7336b482b23b010d0f652b0111d5aec
Michael Dür <62062370+michael-duer@users.noreply.github.com>
2022-08-28 11:52:16 +0200

    add pokemon type colors
    add component containing all pokemon types and according colors
    add color of pokemon type as background color for each card
    minor styling improvement

--- src/components/PokemonInfoCard.js
+++ src/components/PokemonInfoCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { colors } from './pokemon-type-color'
 
 export default function PokemonInfoCard(
   {
@@ -9,15 +10,23 @@ export default function PokemonInfoCard(
     height,
     weight
   }) {
-
+  
   return (
-    <div className='card' key={id}>
+    //use the first type of each pokemon to set the background color by searching the name in 'colors'
+    <div className='card' style={{backgroundColor: colors[types[0].type.name]}}>
       <p className='id'>#{id}</p>
-      <img src={image} alt={name} />
+      <div className='img-container'>
+        <img src={image} alt={name} />
+      </div>
       <p className='name'>{name.toUpperCase()}</p>
       <p className='info'>Type: {types.map((e, index) => {
         //add "/" after each element except for the last one (index=false)
-          return (index ? " / ":"") + e.type.name})}</p>
+          return (
+            <span className='pokemonType' style={{backgroundColor: colors[e.type.name]}} key={index}>
+              {e.type.name} 
+            </span>
+          )
+        })}</p>
       <p className='info'>Height: {height*10}cm</p>
       <p className='info'>Weight: {(weight*0.1).toFixed(2)}kg</p>
     </div>
